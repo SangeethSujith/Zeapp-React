ba43714ddf4257cf2bc731c59d5e4297fe41ada6
Sangeeth Sujith <sangeethps2255@gmail.com>
2023-10-12 16:37:58 +0530

    Login Page Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
+import { Routes, Route } from 'react-router-dom';
+import { ToastContainer } from "react-toastify"
+import 'react-toastify/dist/ReactToastify.css';
+import toasterConfig from "../src/common/renderToaster"
+
+
 import Login from './pages/Login/Login'
 import Dashboard from './pages/Dashboard/Dashboard'
+import CareerEVT from './pages/CareerEVT/CareerEVT'
+import ReasoningTest from './pages/ReasoningTest/ReasoningTest';
+
 function App() {
   return (
-    <Dashboard/>
-  )
+    // <Dashboard/>
+    // <Login/>
+    <>
+    <ToastContainer theme="colored" {...toasterConfig} />
+    <Routes>
+      <Route path='/' element={<Login />} />
+      <Route path='/dashboard' element={<Dashboard />} />
+      <Route path='/career-evaluation' element={<CareerEVT />} />
+      <Route path='/reasoning-test' element={<ReasoningTest/>}/>
+    </Routes>
+    </>
+    // <CareerEVT/>
+    )
 }
 
 export default App
---
+++ src/constants/routepath.js
@@ -0,0 +1,6 @@
+export const routepath={
+    login:'/',
+    dashboard:'/dashboard',
+    careerEVT:'/career-evaluation',
+    reasoning:'/reasoning-test'
+}
