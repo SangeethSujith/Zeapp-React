ddd53bab2e30d406d406eacb456b59a5e111802f
Sangeeth Sujith <sangeethps2255@gmail.com>
2023-10-14 11:15:06 +0530

    Feat: Added rest of the api into the endpoints

--- src/constants/endpoints.js
+++ src/constants/endpoints.js
@@ -1,3 +1,14 @@
 export const endpoints = {
   login: "/do_login",
+  examList:"/exam_list",
+  getCareerClusterExam:"/getCareerClusterExam",
+  getQuestionsByExam:"/getQuestionsByExam",
+  saveExamResults:"/saveExamResults",
+  saveCareerCluster:"/saveCareerCluster",
+  saveCareerInterest:"/saveCareerInterest",
+  savePsycometric:"/savePsycometric",
+  getCareerClusterExam:"/getCareerClusterExam",
+  getCareerSurvey:"/getCareerSurvey",
+  getPsychometricInterest:"/getPsychometricInterest",
+  getCareerExamTypesForUser:"/getCareerExamTypesForUser",
 };
