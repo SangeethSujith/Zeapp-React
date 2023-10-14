0ebf0ea52ff7bde9f495a5805fe4f690b58cb66d
Sangeeth Sujith <sangeethps2255@gmail.com>
2023-10-14 12:16:35 +0530

    Fix: Added href to footer element for playstore

--- src/components/shared/Footer/index.jsx
+++ src/components/shared/Footer/index.jsx
@@ -14,7 +14,9 @@ const Footer = () => {
         </div>
         
         <div className="app-buttons">
+        <a target='_blank' href="https://play.google.com/store/apps/details?id=com.udyata.myapps.zeapp&pli=1">
           <button className="app-button">Download on Play Store</button>
+        </a>
           <button className="app-button">Download on App Store</button>
         </div>
       </div>
