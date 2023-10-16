8a198313c19f395703ddd9c0a21d6a76df1d81bc
Sangeeth Sujith <sangeethps2255@gmail.com>
2023-10-16 14:07:50 +0530

    Feat : Enabled passing params through the URL

--- src/routes/routeMapper.jsx
+++ src/routes/routeMapper.jsx
@@ -157,7 +157,7 @@ const routeMapper = () => {
       ],
     },
     {
-      path: routepath.reasoningExam,
+      path: routepath.reasoningExam+"/:quid",
       element: (
         <PrivateRoute>
           <Outlet />
