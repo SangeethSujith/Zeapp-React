bd9a8ab99c54e024fa91c6b92d67014618be9374
Sangeeth Sujith <sangeethps2255@gmail.com>
2023-10-17 11:20:53 +0530

    Fix : Issues when nevil merged

--- src/components/exams/reasoningexam/index.jsx
+++ src/components/exams/reasoningexam/index.jsx
@@ -1,14 +1,7 @@
 import React, { useEffect, useState } from "react";
-import React, { useEffect, useState } from "react";
 import parseJSON from "../../../utils/jsonparser";
 import Footer from "../../shared/Footer";
 import Timer from "../../shared/Timer";
-import { useParams } from "react-router-dom";
-import { userData } from "../../../utils/loginData";
-import { endpoints } from "../../../constants/endpoints";
-import Axios from "axios";
-import qs from "qs";
-import NumberPad from "./NumberPad";
 import { useParams } from "react-router-dom";
 import { userData } from "../../../utils/loginData";
 import { endpoints } from "../../../constants/endpoints";
@@ -141,7 +134,6 @@ const ReasoningExam = () => {
             </div>
           </div>
         </div>
-        <Footer />
       </div>
     );
   };
